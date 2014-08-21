a68e695518997e585a77ce296e228e8207babd02
Cristian Rojas <cristianguillermorojas@gmail.com>
2014-08-21 10:20:51 -0500

    Initial full project commit

---
+++ superheroes_store/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
+from django.contrib import admin
+
+
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
